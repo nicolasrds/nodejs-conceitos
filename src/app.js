232618312a062518eb8e3a275bc79991c8c4c5b2
Nicolas Rodrigues da Silva <nicolasrds.nr@gmail.com>
2020-05-16 15:53:07 -0300

    Finished first challenge -- Bootcamp Omnistack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,72 @@ app.use(cors());
 
 const repositories = [];
 
+
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repository = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs, 
+    likes: 0
+  };
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: 'repository not found'});
+  };
+  
+  const { title, url, techs } = request.body;
+  const { likes } = repositories[repositoryIndex];
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+  repositories[repositoryIndex] = repository;
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: 'repository not found'});
+  };
+
+  repositories.splice(repositoryIndex,1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: 'repository not found'});
+  };
+
+  const repository =  repositories[repositoryIndex];
+  repository.likes = repository.likes + 1;
+
+  return response.status(201).json(repository);
+
 });
 
 module.exports = app;
